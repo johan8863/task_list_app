f8e6401156968399fd8408cca1db74f5040103e8
Johan Travieso Castro <jtravieso8863@gmail.com>
2024-11-04 22:26:47 -0500

    tasks urls

---
+++ backend/tasks/urls.py
@@ -0,0 +1,14 @@
+"""tasks urls module"""
+
+# django
+from django.urls import path
+
+# local
+from . import views
+
+
+urlpatterns = [
+    path('tasks-list/', views.TaskListView.as_view(), name='tasks_list'),
+    path('tasks-detail/', views.TaskDetailView.as_view(), name='tasks_detail'),
+    path('tasks-list-pending/', views.TaskListPendingView.as_view(), name='tasks_list_pending'),
+]
