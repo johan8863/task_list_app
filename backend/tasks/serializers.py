b561d5b7f1162583dee7231a3198cec512279cf6
Johan Travieso Castro <jtravieso8863@gmail.com>
2024-11-04 21:12:02 -0500

    Task serializer.

---
+++ backend/tasks/serializers.py
@@ -0,0 +1,28 @@
+"""tasks serializer module"""
+
+# third
+from rest_framework import serializers
+
+# local
+from .models import Task
+
+
+class TaskSerializerWrite(serializers.ModelSerializer):
+    """
+    Class to serialize/deserialize Task objects in POST, PUT
+    and DELETE methods, due to the fact that created and updated methods
+    are not editable.
+    """
+    class Meta:
+        model = Task
+        fields = ['id', 'name', 'content']
+
+
+class TaskSerializerRead(serializers.ModelSerializer):
+    """
+    Class to serialize/deserialize Task objects in GET method,
+    all attributes can be retrieved.
+    """
+    class Meta:
+        model = Task
+        fields = ['id', 'name', 'content', 'created', 'updated']
