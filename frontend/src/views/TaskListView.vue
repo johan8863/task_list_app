057d4c59b7be27cd0833aabe6bd9732d76e126fa
Johan Travieso Castro <jtravieso8863@gmail.com>
2024-11-09 09:32:41 -0500

    first rendering of tasks mocking

--- frontend/src/views/TaskListView.vue
+++ frontend/src/views/TaskListView.vue
@@ -1,5 +1,44 @@
-<script setup></script>
+<script setup>
+// vue
+import { ref } from 'vue'
+
+// tasks object
+const tasks = ref([
+  {
+    id: 1,
+    name: 'Kitchen',
+    content: 'Wash the dishes, dry the sink.',
+    done: true,
+  },
+  {
+    id: 2,
+    name: 'Groceries',
+    content: 'Chicken drums.',
+    done: true,
+  },
+  {
+    id: 3,
+    name: 'Painting',
+    content: 'Paint the rooms ASAP.',
+    done: false,
+  },
+  {
+    id: 1,
+    name: 'Accounting',
+    content: 'Transfer 10% to saving credit card.',
+    done: true,
+  },
+])
+</script>
 
 <template>
-  <div>tasks</div>
+  <div>
+    <h2>Your tasks!</h2>
+    <div>
+      <template v-for="task of tasks" :key="task.id">
+        <h4>{{ task.name }}</h4>
+        <p>{{ task.content }}</p>
+      </template>
+    </div>
+  </div>
 </template>
