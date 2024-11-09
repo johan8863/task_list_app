9112454418293dcd83091255da9c645987a69c3d
Johan Travieso Castro <jtravieso8863@gmail.com>
2024-11-09 15:11:37 -0500

    create new task

--- frontend/src/services/task.service.js
+++ frontend/src/services/task.service.js
@@ -1,7 +1,12 @@
-import apiBase from './base.service'
+import apiBase from "./base.service";
 
-const baseTasksListUrl = '/tasks-list'
+const baseTasksListUrl = "/tasks-list";
 
-export const getAllTasks = async () => await apiBase.get(`${baseTasksListUrl}/`)
+export const getAllTasks = async () =>
+  await apiBase.get(`${baseTasksListUrl}/`);
 
-export const getPendingTasks = async () => await apiBase.get(`${baseTasksListUrl}-pending/`)
+export const getPendingTasks = async () =>
+  await apiBase.get(`${baseTasksListUrl}-pending/`);
+
+export const postTask = async (task) =>
+  await apiBase.post(`${baseTasksListUrl}/`, task);
--- frontend/src/views/TaskListView.vue
+++ frontend/src/views/TaskListView.vue
@@ -4,9 +4,14 @@ import { ref, onMounted, computed } from "vue";
 
 // local
 import TaskDetailComponent from "@/components/TaskDetailComponent.vue";
-import { getAllTasks } from "@/services/task.service";
+import { getAllTasks, postTask } from "@/services/task.service";
 
 // tasks object
+const task = ref({
+  name: "",
+  content: "",
+  done: false,
+});
 const tasks = ref([]);
 
 // handlers
@@ -25,6 +30,15 @@ const listAllTasks = async () => {
   tasks.value = response.data;
 };
 
+const createTask = async () => {
+  const response = await postTask(task.value);
+  const newTask = response.data;
+  tasks.value.unshift(newTask);
+  task.value.name = "";
+  task.value.content = "";
+  task.value.done = false;
+};
+
 // Life cycle
 onMounted(async () => {
   await listAllTasks();
@@ -34,16 +48,29 @@ onMounted(async () => {
 <template>
   <div>
     <h2>Your tasks!</h2>
+    <!-- toggling control -->
     <div>
-      <form>
-        <input
-          @change="displayDoneTasks = !displayDoneTasks"
-          type="checkbox"
-          name=""
-          id=""
-        />
-        Show done.
-        <input type="text" />
+      <input
+        @change="displayDoneTasks = !displayDoneTasks"
+        type="checkbox"
+        name=""
+        id="displayDone"
+      />
+      <label for="displayDone">Show done.</label>
+    </div>
+    <div>
+      <form @submit.prevent="createTask">
+        <!-- name control -->
+        <input type="text" v-model="task.name" placeholder="name" />
+
+        <!-- content control -->
+        <input type="text" v-model="task.content" placeholder="content" />
+
+        <!-- done control -->
+        <input type="checkbox" v-model="task.done" /> done
+
+        <!-- buttons -->
+        <button type="submit">Add</button>
       </form>
     </div>
     <div>
