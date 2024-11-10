39f678f5942d660359a9e8a482206d300d36e1b1
Johan Travieso Castro <jtravieso8863@gmail.com>
2024-11-10 15:17:48 -0500

    installed bootsrap icons to the main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
 import { createApp } from "vue";
